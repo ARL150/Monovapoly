acbbdf4ae05e28ae160728cfc9ce1759c1b24292
Jesus Abraham Robledo Lopez <abrahamrobledo0402@gmail.com>
2025-04-18 12:38:21 -0600

    cambio menu responsivo 2

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,13 @@
-    const toggle = document.querySelector('.menu-toggle');
-    const nav = document.querySelector('.navigation');
-    const closeBtn = document.querySelector('.close-btn');
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.navigation');
+const closeBtn = document.querySelector('.close-btn');
 
-    toggle.addEventListener('click', () => {
-        nav.classList.add('active');
-    });
+toggle.addEventListener('click', () => {
+    nav.classList.add('active');
+    toggle.style.display = 'none'; // Oculta el botón al abrir
+});
 
-    closeBtn.addEventListener('click', () => {
-        nav.classList.remove('active');
-    });
-
-
+closeBtn.addEventListener('click', () => {
+    nav.classList.remove('active');
+    toggle.style.display = 'flex'; // Muestra el botón al cerrar
+});
