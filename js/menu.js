df8c3308f684f879e5b42f3962b0e9b4a339c394
Jesus Abraham Robledo Lopez <abrahamrobledo0402@gmail.com>
2025-04-18 12:06:31 -0600

    cambio index insta

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,18 @@
-    const menuToggle = document.getElementById('menu-toggle');
-    const mobileMenu = document.getElementById('mobile-menu');
+const menuToggle = document.querySelector('.menu-toggle');
+const navigation = document.querySelector('.navigation');
+const closeBtn = document.querySelector('.close-btn');
 
-    // Función para abrir/cerrar el menú
-    menuToggle.addEventListener('click', () => {
-        mobileMenu.classList.toggle('active');
-    });
+menuToggle.addEventListener('click', () => {
+    navigation.classList.toggle('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    navigation.classList.remove('active');
+});
+
+document.addEventListener('click', (event) => {
+    if (!navigation.contains(event.target) && !menuToggle.contains(event.target)) {
+        navigation.classList.remove('active');
+    }
+});
+
